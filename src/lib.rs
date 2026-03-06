db0bf25f385962bd5df8b3298df243706c0b76e3
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2026-03-06 10:46:06 -0500

    feat: MAJOR + PATCH versions (#49)

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ pub mod mpmc;
 mod shmem;
 pub mod spsc;
 
-pub(crate) const VERSION: u8 = 1;
+pub(crate) const VERSION_MAJOR: u8 = 2;
+pub(crate) const VERSION_PATCH: u8 = 0;
+pub(crate) const VERSION: u16 = (VERSION_MAJOR as u16) << 8 | VERSION_PATCH as u16;
 
 /// `AtomicUsize` with 64-byte alignment for better performance.
 #[derive(Default)]
